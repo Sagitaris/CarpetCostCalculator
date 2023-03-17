573374fd0bf77719c2184ef7f5472a44d6db90a1
Anna Adamik <quenthel5@gmail.com>
2023-03-17 11:26:28 +0100

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public double getTotalCost() {
+        return floor.getArea() * carpet.getCost();
+    }
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+}
